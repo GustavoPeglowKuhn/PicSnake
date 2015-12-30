608c383a76670d2870726ac9e9ed7f5a59a8a4aa
Gustavo <gustavowolgep@gmail.com>
2015-12-30 01:55:44 -0200

    v0.0.3

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 //bibliotecas
 #include <GLCD.c>
 
+#define width     128
+#define height    64
+#define initSize  10
 #define direita   0
 #define dir       pin_c1
 #define cima      1
@@ -29,7 +32,7 @@ struct Dot{
 struct Dot snake[128];
 struct Dot point;
 int8   sentido;
-int8   size=10;
+int8   size=initSize;
 
 void mov(){
    int8 i;
@@ -38,32 +41,32 @@ void mov(){
    aux.y = snake[0].y;
    switch (sentido){
       case direita:
-         if(aux.x==127)
+         if(aux.x==width-1)
             aux.x=0;
          else
             aux.x++;
       break;
       case cima:
          if(aux.y==0)
-            aux.y=63;
+            aux.y=height-1;
          else
             aux.y--;
       break;
       case esquerda:
          if(aux.x==0)
-            aux.x=127;
+            aux.x=width-1;
          else
             aux.x--;
       break;
       case baixo:
-         if(aux.y==63)
+         if(aux.y==height-1)
             aux.y=0;
          else
             aux.y++;
       break;
    }
    for(i=1;i<size;i++){
-      if(aux.x==snake[i].x && aux.y==snake[i].y){
+      if(aux.x==snake[i].x && aux.y==snake[i].y){     //end game
          glcd_text57(0,0,yl,2,1);
          delay_ms(1000);
          return;
@@ -71,7 +74,14 @@ void mov(){
    }
    if(aux.x==point.x && aux.y==point.y){
       size++;
+      point.x = get_timer0() % width;
+      point.y = get_timer0() % height;
+      glcd_pixel(point.x,point.y,1);
    }
+   else{
+      glcd_pixel(snake[size-1].x,snake[size-1].y,0);
+   }
+   glcd_pixel(aux.x,aux.y,1);
    for(i=size;i>0;i--){
       snake[i].x=snake[i-1].x;
       snake[i].y=snake[i-1].y;
@@ -80,14 +90,14 @@ void mov(){
    snake[0].y=aux.y;
 }
 
-void draw(){
+/*void draw(){
    int8 i;
    glcd_fillScreen(0);
    for(i=1;i<size;i++){
       glcd_pixel(snake[i].x,snake[i].y,1);
    }
    glcd_pixel(point.x,point.y,1);
-}
+}*/
 
 void main(){
    setup_adc_ports(AN0);
@@ -103,17 +113,19 @@ void main(){
    glcd_init(ON);   //inicializa o display
    glcd_fillScreen(0); //limpa display inteiro
 
-   snake[0].x = 64;
+   snake[0].x = 10;
    snake[0].y = 32;
-   snake[1].x = 63;
+   /*snake[1].x = 63;
    snake[1].y = 32;
    snake[2].x = 62;
    snake[2].y = 32;
    snake[3].x = 61;
-   snake[3].y = 32;
+   snake[3].y = 32;*/
    
-   point.x = 50;
-   point.y = 32;
+   point.x = get_timer0() % width;
+   point.y = get_timer0() % height;
+   glcd_pixel(point.x,point.y,1);
+   
    while(TRUE){
       if(input(dir)){
          if(sentido!=esquerda)
@@ -132,61 +144,6 @@ void main(){
             sentido = baixo;
       }
       mov();
-      draw();
-      delay_ms(10);
+      delay_ms(100);
    }
 }
-/*    int n;               //circle test
-      for(n=0;n<32;n+=3){
-         glcd_circle(64, 32, n, NO, 1);
-         delay_ms(100);
-      }
-      glcd_fillScreen(1);
-      for(n=0;n<32;n+=3){
-         glcd_circle(64, 32, n, NO, 0);
-         delay_ms(100);
-      }
-      glcd_fillScreen(0);*/
-      
-/*    char Str1[20];       //text test
-      Str1 = "Loren Ipsun";
-      int n;
-      for(n=0;n<4;n++){
-         glcd_fillScreen(0);
-         glcd_text57(0,0,Str1,n,1);
-         delay_ms(500);
-      }*/
-      
-/*    int x,y;       //lazy clock
-      float a;
-      for(a=0;a<6.2831;a+=0.0523598){
-         y=(float)-30*cos(a);
-         x=(float)30*sin(a);
-         y+=32;
-         x+=64;
-         glcd_fillScreen(0);
-         glcd_circle(64, 32, 32, NO, 1);
-         glcd_line(64,32,x,y,1);
-         delay_ms(500);
-      }*/
-      
-/* set_adc_channel(0);     //osciloscope
-   int8 xa,x,n=0;
-   char pt[10],pv[10];
-   pt = "100ms";
-   pv = "78.25v";
-   glcd_text57(0,57,pv,1,1);
-   glcd_text57(90,57,pt,1,1);
-   while(TRUE){
-      n++;
-      if(n>127){
-         n=1;
-         glcd_fillScreen(0);
-         glcd_text57(0,57,pv,1,1);
-         glcd_text57(90,57,pt,1,1);
-      }
-      xa=x;
-      x=read_adc()/4;
-      glcd_line(n-1,xa,n,x,1);
-      delay_ms(100);
-   }*/
